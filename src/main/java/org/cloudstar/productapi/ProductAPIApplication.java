8a7961c4d9f47c1ef886dc421036b636dcb686ab
Anuran Datta <anurdatt@in.ibm.com>
2019-12-08 20:35:13 +0530

    cheat to see table names in DB

--- src/main/java/org/cloudstar/productapi/ProductAPIApplication.java
+++ src/main/java/org/cloudstar/productapi/ProductAPIApplication.java
@@ -1,14 +1,31 @@
 package org.cloudstar.productapi;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class ProductAPIApplication {
+public class ProductAPIApplication implements CommandLineRunner{
 
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProductAPIApplication.class, args);
 	}
 	
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		List<Map<String, Object>> a = jdbcTemplate.queryForList("show tables");
+		a.forEach(System.out::println);
+	}
 	
 }
